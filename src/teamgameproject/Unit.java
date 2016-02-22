5216366a581206f19b520c0d6044643e33c127ed
ShiFan <ShiFan@104.39.94.245>
2016-02-22 17:48:56 -0500

    added Peon (basic unit) and updated unit class

--- src/teamgameproject/Unit.java
+++ src/teamgameproject/Unit.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package teamgameproject;
 
-/**
- *
- * @author rwb5529
- */
-public class Unit {
-    
+public abstract class Unit {
+
+    private int health;
+    private int attack;
+    private int range;
+    private String details;
+    private int posX;
+    private int posY;
+    private int mobility;
+
+    public Unit(int health, int attack, int range, String details, int posX, int posY, int mobility) {
+        this.health = health;
+        this.attack = attack;
+        this.range = range;
+        this.details = details;
+        this.posX = posX;
+        this.posY = posY;
+        this.mobility = mobility;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void moveUnit(int newPosX, int newPosY) {
+        if (Math.abs(posX - newPosX) <= mobility) {
+            posX = newPosX;
+        }
+        if (Math.abs(posY - newPosY) <= mobility) {
+            posY = newPosY;
+        }
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 }
