12c2912bfa7a0f3fe147eea3e738a6169e95d4d0
Striped <Striped@104.39.98.56>
2016-02-26 12:25:44 -0500

    Implemented Action and Mouse Listener

--- src/teamgameproject/GameBoardPanel.java
+++ src/teamgameproject/GameBoardPanel.java
@@ -7,7 +7,12 @@ package teamgameproject;
 
 import static com.sun.java.accessibility.util.AWTEventMonitor.addActionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
@@ -15,11 +20,12 @@ import javax.swing.border.LineBorder;
  *
  * @author rwb5529
  */
-public class GameBoardPanel extends JPanel{
+public class GameBoardPanel extends JPanel implements MouseListener, ActionListener{
     
     private int xAxis = 10;
     private int yAxis = 10;
     
+    private ArrayList<Unit> units = new ArrayList<Unit>();
     
     private GameBoardSpace[][] boardSpaces = new GameBoardSpace[xAxis][yAxis];
     
@@ -36,7 +42,7 @@ public class GameBoardPanel extends JPanel{
         Insets buttonSeperation = new Insets(0,0,0,0);
         for (int i = 0; i < boardSpaces.length; i++){
             for (int j = 0; j < boardSpaces[i].length; j++){
-                GameBoardSpace space = new GameBoardSpace();
+                GameBoardSpace space = new GameBoardSpace(j, i);
                 space.setMargin(buttonSeperation);
                 ImageIcon spaceIcon = new ImageIcon(new BufferedImage(50,50, BufferedImage.TYPE_INT_ARGB));
                 space.setIcon(spaceIcon);
@@ -45,9 +51,44 @@ public class GameBoardPanel extends JPanel{
                 space.setBackground(Color.WHITE);
                 boardSpaces[j][i] = space;
                 this.add(boardSpaces[j][i]);
-                addActionListener(boardSpaces[j][i]);
+                boardSpaces[j][i].addMouseListener(this);
+                
+                
             }
+            }
+        
+            
         }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("mouse clicked");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("mouse pressed");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("mouse released");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("mouse entered");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("mouse exited");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("action performed");
+    }
         
     }
-}
+
--- src/teamgameproject/GameBoardSpace.java
+++ src/teamgameproject/GameBoardSpace.java
@@ -15,11 +15,28 @@ import javax.swing.*;
 public class GameBoardSpace extends JButton implements ActionListener{
     private int xCord;
     private int yCord;
+    private boolean isOccupied;
     
-    public GameBoardSpace(){
+    public GameBoardSpace(int x, int y){
+        this.xCord = x;
+        this.yCord = y;
+        this.isOccupied = false;
+    }
+    
+    public void addSpaceToArray(){
         
     }
-
+    
+    public int getXCord(){
+        return this.xCord;
+    }
+    
+    public int getYCord(){
+        return this.yCord;
+    }
+    
+    
+    
     @Override
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
