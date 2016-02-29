7518c17453b1a1186d1598334f6176ebc3428d1e
Colin Burns <cfb5125@psu.edu>
2016-02-29 14:56:34 -0500

    Added Peon test to simulate an attack, decreasing health and increasing the attack attribute.

--- src/teamgameproject/Peon.java
+++ src/teamgameproject/Peon.java
@@ -10,5 +10,12 @@ public class Peon extends Unit{
         super(1, 1, 1, "A simple unit", /*posX, posY,*/ 1, new ImageIcon("src/images/peon.png") );
         
     }
+    
+    public void attackPeon(Peon attacker, Peon victim){
+        int healthAfterAttack = 0;
+        victim.setHealth(healthAfterAttack);
+        int attackIncrease = 2;
+        attacker.setAttack(attackIncrease);
+    }
    
 }
