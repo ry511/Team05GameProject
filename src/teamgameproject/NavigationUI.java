ad24e687120031c1902bd47760ae5fc51a84ec36
jensun93 <jensun93@gmail.com>
2016-03-02 14:32:57 -0500

    Add Option and How to play Add classes for that

--- src/teamgameproject/NavigationUI.java
+++ src/teamgameproject/NavigationUI.java
@@ -4,6 +4,7 @@ package teamgameproject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  *
@@ -13,8 +14,9 @@ public class NavigationUI extends JFrame{
     
     private NavigationCntl navigationCntl = null;
   
-    
+    private JButton optionButton;
     private JButton startButton;
+    private JButton instruction;
     private JPanel buttonPanel;
 
     public NavigationUI(NavigationCntl parentNavigationCntl) {
@@ -23,14 +25,16 @@ public class NavigationUI extends JFrame{
     }
     
     public void initCustomerComponents(){
-        this.setSize(500, 500);
+        this.setSize(1000, 1000);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
 
         
-        
+        optionButton = new JButton("Option");
         startButton = new JButton("Start");
+        instruction = new JButton("How To Play");
+        
         startButton.addActionListener(new java.awt.event.ActionListener(){
             public void actionPerformed(java.awt.event.ActionEvent evt){
                  startButtonActionPerformed(evt);
@@ -39,15 +43,63 @@ public class NavigationUI extends JFrame{
         buttonPanel = new JPanel();
         
         buttonPanel.add(startButton);
+        
+        
+       
+        
+        this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(instruction, BorderLayout.SOUTH);
+        buttonPanel.add(optionButton);
+        
+         this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.add(instruction);
+        
         this.getContentPane().setLayout(new BorderLayout());
         this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-        
-        
+       
          
-          startButton.setFont(startButton.getFont().deriveFont(18.0f));
         
-        startButton.setPreferredSize(new Dimension(100, 100));
+        startButton.setFont(startButton.getFont().deriveFont(36.0f));
+        startButton.setPreferredSize(new Dimension(300, 200));
+        
+        optionButton.setFont(optionButton.getFont().deriveFont(36.0f));
+        optionButton.setPreferredSize(new Dimension(300, 200));
+        
+        instruction.setFont(instruction.getFont().deriveFont(36.0f));
+        instruction.setPreferredSize(new Dimension(300, 200));
+        
+        
+        
+        optionButton.addActionListener(new java.awt.event.ActionListener(){
+            public void actionPerformed(java.awt.event.ActionEvent evt){
+                 optionButtonActionPerformed(evt);
+            }
+        });
+      //  buttonPanel = new JPanel();
+        
+       
     }
+    
+    
+        private void instructionButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        
+        MyJFrame mjf = new MyJFrame();
+        this.setVisible(false);
+        mjf.setVisible(true);
+        mjf.setLocationRelativeTo(null);
+        
+        }
+    
+    
+        private void optionButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        
+        MyJFrame mjf = new MyJFrame();
+        this.setVisible(false);
+        mjf.setVisible(true);
+        mjf.setLocationRelativeTo(null);
+        
+        }
     
         private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
         
